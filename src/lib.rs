4892fb5e10384585c4142bfbdda849b5b84625d6
Vincent Esche <regexident@gmail.com>
2023-01-10 00:20:12 +0100

    Warn on missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 //! An efficient `no_std`-compatible implementation of a quadrature decoder,
 //! based on a finite-state-transducer with support for different step-modes.
+#![warn(missing_docs)]
 #![cfg_attr(not(test), no_std)]
