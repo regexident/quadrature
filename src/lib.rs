0c5ed226946b89e145505ffa84867f4044680d36
Vincent Esche <regexident@gmail.com>
2023-01-10 13:26:14 +0100

    Make crate `no_std` compatible

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
-//! An efficient `no_std`-compatible implementation of a quadrature decoder
+//! An efficient `no_std`-compatible implementation of a quadrature decoder,
 //! based on a finite-state-transducer with support for different step-modes.
+#![cfg_attr(not(test), no_std)]
