05e1086300c4915b0e24f3c71d69c359a8699799
Vincent Esche <regexident@gmail.com>
2025-01-24 11:17:48 +0100

    Fix a few typos

--- quadrature-encoder/src/traits.rs
+++ quadrature-encoder/src/traits.rs
@@ -1,5 +1,5 @@
-// Polled pins must impliment ehal v1.0.0 InputPin trait,
-// either directly or via embadded-hal-compat Forward-ing.
+// Polled pins must implement the `InputPin` trait from embedded-hal v1.0.0,
+// either directly or via `embedded-hal-compat` forward-ing.
 pub use eh1::digital::InputPin;
 use embedded_hal_compat::eh1_0 as eh1;
 
